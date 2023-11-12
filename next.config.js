5077098ebbacbc28a3628603b6fe46d53b5527c0
Filip Stojanovic <philsty92@gmail.com>
2023-11-12 09:47:32 -0500

    Added device sizes and img types  to next config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
 		accessToken: process.env.CONTENTFUL_ACCESS_KEY
 	},
 	images: {
-		domains: ['images.ctfassets.net', 'videos.ctfassets.net']
+		domains: ['images.ctfassets.net', 'videos.ctfassets.net'],
+		formats: ['image/avif', 'image/webp'],
+		deviceSizes: [640, 768, 1024, 1280, 1600]
+		// imageSizes: [16, 32, 48, 64, 96]
 	}
 }
 
